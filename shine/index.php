e25c10b239b0598f189712a528d0c94329338f9c
Thomas Manu <tomkm22@gmail.com>
2018-03-30 08:23:41 -0400

    Test Database connection

--- shine/index.php
+++ shine/index.php
@@ -6,38 +6,37 @@
     <body>
         <?php
 
+            $serverName = "10.14.2.76";
+            $username = "adamsbala";
+            $port = "1433";
+            $password = "adamsbala";
+            $dbname = "PHP";
+            $connectionInfo = array("Database"=>$dbname,
+                                    "UID"=>$username,
+                                    "PWD"=>$password,
+                                    "Port"=>$port);
+            // Create connection
+            $conn = sqlsrv_connect($serverName, $connectionInfo);
+            // Check connection
+            if( $conn ) {
+                echo "Connection established.<br />";
+            }else{
+                echo "Connection could not be established.<br />";
+                die( print_r( sqlsrv_errors(), true));
+            }
+            /*$sql = "SELECT * from student";
+            $result = $conn->query($sql);
 
-$serverName = "10.14.2.76";
-$port = "1433";
-$username = "adamsbala";
-$password = "adamsbala";
-$dbname = "PHP";
-$connectionInfo = array("Database"=>$dbname,
-                        "UID"=>$username,
-                        "PWD"=>$password,
-                        "Port"=>$port);
-// Create connection
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-// Check connection
-if( $conn ) {
-     echo "Connection established.<br />";
-   }else{
-     echo "Connection could not be established.<br />";
-     die( print_r( sqlsrv_errors(), true));
-   }
-$sql = "SELECT * from student";
-$result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+                    echo "<br> id: ". $row["ID"]. " - Name: ". $row["name"]. " " . $row["dept_name"] . "<br>";
+                }
+            } else {
+                echo "0 results";
+            }*/
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "<br> id: ". $row["ID"]. " - Name: ". $row["name"]. " " . $row["dept_name"] . "<br>";
-    }
-} else {
-    echo "0 results";
-}
-
-$conn->close();
+            $conn->close();
         ?>
         <!-- Footer -->
         <footer>
