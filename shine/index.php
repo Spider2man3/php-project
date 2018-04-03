bcbba76e7f498876218d256a052ce3a12a27fd2e
Adams Bala <adams.alok@gmail.com>
2018-04-03 10:43:17 -0400

    proper quer234

--- shine/index.php
+++ shine/index.php
@@ -58,7 +58,7 @@ Order by dept_name";
                $list = array();
 
                foreach ($values as $key => $value) {
-                  $values[$value]['dept_name'] = $values[$value]['ratio'];
+                  echo $values[$value]['dept_name'];
                }
                echo "<pre>";
                print_r($values);
