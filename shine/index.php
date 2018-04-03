27b75ef1d180b53318e653146448fad6eb45797b
Adams Bala <adams.alok@gmail.com>
2018-04-03 15:17:53 -0400

    id2

--- shine/index.php
+++ shine/index.php
@@ -125,8 +125,8 @@ pivot
 </div>
                       <!-- Footer -->
                       <footer>
-                          <a href="../index.php">Home</a>
-                          <a href="../maze/index.php">Shine Project</a>
+                          <a href='../index.php'>Home</a>
+                          <a href='../maze/index.php'>Shine Project</a>
                       </footer>
               	</body>
               </html>>";
